50dc4176a84a57be40a2a5c9eef5b3b53c7e0655
Abhilash Muraleedharan <abhilashmuraleedharan@outlook.com>
2021-03-02 23:59:21 +0530

    variables

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int firstNumber = 13;
+        int secondNumber = 12;
+        int thirdNumber = 6;
+        System.out.println(firstNumber + secondNumber + thirdNumber);
     }
 }
